fc9e766d27d031e9d9cd85e4491009bca7c6d84c
Ulises <ulises.argueta95@gmail.com>
2023-05-21 12:10:31 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() { 
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* add the <Home> component here */Home()}
+      {/* add your <About> component here */About()}
     </div>
   );
 }
